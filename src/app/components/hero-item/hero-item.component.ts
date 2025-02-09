4bd4df374567afc51ad21d9f55cbef43b9892e2b
Minuke <nakamadeath@gmail.com>
2025-02-09 23:54:50 +0100

    add hero item component with power stats and styling

---
+++ src/app/components/hero-item/hero-item.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { Hero, PowerStat } from '../../shared/interfaces/hero.interface';
+
+@Component({
+  selector: 'app-hero-item',
+  imports: [],
+  templateUrl: './hero-item.component.html',
+  styleUrl: './hero-item.component.scss'
+})
+export class HeroItemComponent {
+
+  hero: Hero = {
+    id: 620,
+    name: "Spider-Man",
+    powerstats: {
+      intelligence: 96,
+      strength: 55,
+      speed: 67,
+      durability: 75,
+      power: 74,
+      combat: 85,
+    },
+    image: "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/sm/620-spider-man.jpg",
+    alignment: "good",
+  }
+
+  isHeroVillain = this.hero.alignment === "bad";
+
+  decrementPowerStats(powerstat: PowerStat): void {
+    const value = this.hero.powerstats[powerstat];
+    if(value > 0) {
+      this.hero.powerstats[powerstat]--;
+    }
+  }
+
+  incrementPowerStats(powerstat: PowerStat): void {
+    const value = this.hero.powerstats[powerstat];
+    if(value < 100) {
+      this.hero.powerstats[powerstat]++;
+    }
+  }
+  
+}
