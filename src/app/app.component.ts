842d09e60449c62e6f3257f9a132f006250838e8
Minuke <nakamadeath@gmail.com>
2025-02-22 13:09:54 +0100

    add header and footer components with Tailwind CSS integration and update app layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component, inject } from '@angular/core';
-import { HeroItemComponent } from './components/hero-item/hero-item.component';
 import { HeroListComponent } from './components/hero-list/hero-list.component';
 import { HeroNewComponent } from './components/hero-new/hero-new.component';
 import { Hero } from './shared/interfaces/hero.interface';
 import { HeroService } from './shared/services/hero.service';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeroListComponent, HeroNewComponent],
+  imports: [HeroListComponent, HeroNewComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
